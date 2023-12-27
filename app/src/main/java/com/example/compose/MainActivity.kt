c2159d274bc507c5573f0c7290b46d5ff49fb910
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-12-27 17:59:20 -0300

    gerenciamento de estados

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,29 +3,33 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
-import com.example.compose.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    // Greeting("Android")
                     MainScreen()
                 }
             }
@@ -45,12 +49,98 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun MainScreen() {
-    Column(modifier = Modifier
-        .background(Color.White)
-        .fillMaxSize()) {
+    //criando estado
+    var clicks by remember {
+        mutableStateOf(0)
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        ClickCounter(clicks = clicks) {
+            clicks++
+        }
+        HelloContent()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HelloContent() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        var name by remember {
+            mutableStateOf("")
+        }
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello, $name!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
+        //borda, etc
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") }
+        )
+    }
+}
+
+
+@Composable
+fun ClickCounter(clicks: Int, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text = "I've been clicked $clicks times.")
+    }
+}
+
+data class Dog(
+    val name: String,
+    val breed: String
+)
+
+//Column  {
+//    DogCard(dog = Dog("Maggie", "Vira-Lata"),
+//        modifier = Modifier.background(Color.Green)
+//    )
+//    DogCard(dog = Dog("Dayse", "Vira-Lata"),
+//        modifier = Modifier.background(Color.Blue)
+//
+//    )
+//}
+@Composable
+fun DogCard(dog: Dog, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxWidth()
+
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_dog),
+            contentDescription = null,
+            modifier = Modifier.size(72.dp)
+            //deixar a imagem arredondada
+            // .clip(CircleShape)
+
+        )
+        Column {
+            Text(text = dog.name)
+            Text(text = dog.breed)
+        }
+
+    }
+}
+
+
+@Composable
+private fun TestingColumns() {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
+    ) {
         Text(
             text = "Hello world!!", maxLines = 3,
             style = MaterialTheme.typography.bodyLarge,
@@ -66,28 +156,17 @@ fun MainScreen() {
                 .background(Color.DarkGray)
                 .fillMaxWidth()
         )
-        
+
         Row {
-            Text(text = "Text 3",
+            Text(
+                text = "Text 3",
                 Modifier.padding(end = 30.dp)
-                )
+            )
 
             Text(text = "Text 4")
         }
-        
-    }
 
-//    Button(onClick = { /*TODO*/ },
-//        colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow)) {
-//
-//        Icon(
-//            painter = painterResource(id = R.drawable.ic_star),
-//            contentDescription = null,
-//            tint = Color.Black
-//        )
-//        Text(text = "Confirmar",
-//            color = Color.Black)
-//    }
+    }
 }
 
 @Preview(showBackground = true)
@@ -112,3 +191,15 @@ fun GreetingPreview() {
         Greeting("Bruno")
     }
 }
+
+//    Button(onClick = { /*TODO*/ },
+//        colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow)) {
+//        Icon(
+//            painter = painterResource(id = R.drawable.ic_star),
+//            contentDescription = null,
+//            tint = Color.Black
+//        )
+//        Text(text = "Confirmar",
+//            color = Color.Black)
+//    }
+
